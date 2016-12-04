3f4b071fa26383ad893a3319651f2af8a2925bca
Qianfan Ma <melvinma88@yahoo.com>
2016-12-04 11:57:22 -0800

    do

--- src/python-src/examples/speak.py
+++ src/python-src/examples/speak.py
@@ -1,4 +1,4 @@
 from gtts import gTTS
 blabla = ("You have been a good boy Have a lollipop")
 tts = gTTS(text=blabla, lang='en-au')
-tts.save("/Users/qianfan/tmp/test.mp3")
+tts.save("/tmp/test.mp3")
