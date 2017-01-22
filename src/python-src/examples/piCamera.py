206b766cf1230199682d237cc6375a22892fd5ee
Steeeal <xkevinma@gmail.com>
2017-01-21 16:21:04 -0800

    camera is working on py

---
+++ src/python-src/examples/piCamera.py
@@ -0,0 +1,7 @@
+import picamera
+
+camera = picamera.PiCamera()
+
+camera.capture('/tmp/cap.jpg')
+
+
--- src/python-src/examples/speak.py
+++ src/python-src/examples/speak.py
@@ -4,5 +4,5 @@ import sys
 print(sys.version)
 
 blabla = ("You have been a good boy Have a lollipop")
-tts = gTTS(text=blabla, lang='en-au')
+tts = gTTS(text=blabla, lang='en-US')
 tts.save("/tmp/test.mp3")
