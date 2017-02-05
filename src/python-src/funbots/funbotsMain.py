e68d5d1b8f6c70c0192f2bcb054b02253ff0856d
Steeeal <xkevinma@gmail.com>
2017-02-04 16:37:21 -0800

    button is working

--- src/python-src/funbots/funbotsMain.py
+++ src/python-src/funbots/funbotsMain.py
@@ -20,24 +20,41 @@ import RPi.GPIO as GPIO
 ##   4. then depend on the result, respond.
 ##   5. then the system could response to the button click again.
 
-def main() :
-
-    while True :
-        #assuming the script to call is long enough we can ignore bouncing
-        if (GPIO.input(buttonPin)) and not processing:
-            #this is the script that will be called (as root)
-            processing = True
-            print("Button pressed")
-            processing = False
 
 def setup() :
-    global processing = False
+    
     #adjust for where your switch is connected
-    global buttonPin = 17
+    global buttonPin
+    buttonPin = 17
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(buttonPin,GPIO.IN)
 
-    global camera = picamera.PiCamera()
+    global camera
+    camera = picamera.PiCamera()
 
 def takePhoto(photoPath):
     camera.capture(photoPath)
+
+
+def main() :
+    print("main before true")
+    counter = 0
+    processing = False
+    while True :
+        ##counter = counter + 1
+        ##if counter % 1000 == 0:
+        ##     print("main inside true")
+        #assuming the script to call is long enough we can ignore bouncing
+        if (not GPIO.input(buttonPin)) and not processing:
+            #this is the script that will be called (as root)
+            processing = True
+            print("Button pressed")
+            
+            ## sleep 1 second
+            time.sleep(1)
+            processing = False
+
+print("before setup")
+setup()
+print("before main")
+main()
